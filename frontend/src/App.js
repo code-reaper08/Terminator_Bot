75fd77aa250d75e0465fd15d2577f27ffbc77a5f
1192805 <morasachandrasuryaprakash@gmail.com>
2023-09-26 21:19:36 +0530

    feer(chandra)added registeration page and app routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+import Resignation from './Pages/Resignation';
+import ResetPassword from './Pages/ResetPassword';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Register/>}/>
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path={"/resetPassword"} element={<ResetPassword/>}/>
+        <Route exact path={"/resignation"} element={<Resignation/>}/> 
+      </Routes>
+    </Router>
     </div>
   );
 }
-
 export default App;
