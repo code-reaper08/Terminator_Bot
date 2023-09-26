6de0c4f6cc3525c1b0a73468f0466368fdd2d291
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2023-09-26 13:20:16 +0530

    init: add directory structure

---
+++ backend/src/main/java/com/terminator_bot/terminator_bot/TerminatorBotApplication.java
@@ -0,0 +1,13 @@
+package com.terminator_bot.terminator_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminatorBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminatorBotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/terminator_bot/terminator_bot/TerminatorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terminator_bot.terminator_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminatorBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
