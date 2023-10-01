c24a6d4c3a5454bce6be00c8ecb0c13516e3abbd
1192827 <jaswanthyerramsetti@gmail.com>
2023-10-01 12:47:06 +0530

    feat:(jaswanth) added navbar and updated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,32 @@ import RegisterSteps from "./Pages/RegisterSteps/RegisterSteps";
 import Login from "./Pages/Login/Login";
 import Resignation from "./Pages/Resignation/Resignation";
 import ResetPassword from "./Pages/ResetPassword/ResetPassword";
-import Navbar from "./Pages/Navbar";
-
+import HomePage from "./Pages/HomePage/Navbar";
+import { PrivateRoute } from "./Components/PrivateRoute";
 import "./App.css";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Aboutus from "./Pages/AboutUs/Aboutus";
-
+import Home from "./Pages/HomePage/Home";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Navbar />} />
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/home" element={<Home/>} />
           <Route exact path="/register" element={<RegisterSteps />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path={"/resetPassword"} element={<ResetPassword />} />
           <Route exact path={"/resignation"} element={<Resignation />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route
+            exact
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
           <Route exact path="/aboutus" element={<Aboutus />} />
         </Routes>
       </Router>
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from './Footer';
+import Home from './Home';
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div >
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></link>
+            <nav style={{ backgroundColor: '#5A287D' }} className="navbar navbar-expand-lg navbar-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand mx-2" href="#">
+                        <img src="https://www.natwestgroup.com/content/dam/championlogos/Logo_Wh_NatWestGroupColleague-Hor.svg" alt="Logo" height="50" />
+                    </a>
+                    <button className="navbar-toggler"
+                        type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                            <Link className='nav-link active' to="/home">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link className='nav-link active' to="/aboutus">About Us</Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link className='nav-link active' to="/career">Careers</Link>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                        <Link className="btn btn-primary ternary-bg" to="/login">Login</Link>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+            <Home/>
+            <Footer/>
+        </div>
+    );
 }
