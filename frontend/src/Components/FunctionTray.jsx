1d17feba18dddbd854370080a0177b800b7abf6b
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2023-10-05 14:06:04 +0530

    feat: connect functiontray with user microservice

--- frontend/src/Components/FunctionTray.jsx
+++ frontend/src/Components/FunctionTray.jsx
@@ -11,145 +11,145 @@ export default function FunctionTray({ requestsArr }) {
 
   const handleSL = async (requester_id, request_id) => {
     let result = {};
-    let request = {};
+    // let request = {};
     await axios
-      .get(`http://localhost:4000/users/${cuur_user.id}`)
+      .get(`http://localhost:8081/person/${requester_id}`)
       .then((res) => (result = res.data));
-    await axios
-      .get(`http://localhost:4000/resignation_requests/${request_id}`)
-      .then((res) => {
-        request = res.data;
-        console.log(res.data);
-      });
+    // await axios
+    //   .get(`http://localhost:4000/resignation_requests/${request_id}`)
+    //   .then((res) => {
+    //     request = res.data;
+    //     console.log(res.data);
+    //   });
     result.submittedLaptop = true;
-    request.requester.submittedLaptop = true;
+    // request.requester.submittedLaptop = true;
     await axios
-      .put(`http://localhost:4000/users/${requester_id}`, result)
+      .put(`http://localhost:8081/person/updateRequest/${requester_id}`, result)
       .then((res) => {
         // localStorage.setItem("user", res.data)
         console.log(res.data);
         localStorage.setItem("user", JSON.stringify(res.data));
-      });
-    await axios.put(
-      `http://localhost:4000/resignation_requests/${request_id}`,
-      request
-    );
+      }).catch((err) => console.log(err))
+    // await axios.put(
+    //   `http://localhost:4000/resignation_requests/${request_id}`,
+    //   request
+    // );
     window.location.reload()
   };
 
   const handleSM = async (requester_id, request_id) => {
     let result = {};
-    let request = {};
+    // let request = {};
     await axios
-      .get(`http://localhost:4000/users/${cuur_user.id}`)
+      .get(`http://localhost:8081/person/${requester_id}`)
       .then((res) => (result = res.data));
-    await axios
-      .get(`http://localhost:4000/resignation_requests/${request_id}`)
-      .then((res) => {
-        request = res.data;
-        console.log(res.data);
-      });
+    // await axios
+    //   .get(`http://localhost:4000/resignation_requests/${request_id}`)
+    //   .then((res) => {
+    //     request = res.data;
+    //     console.log(res.data);
+    //   });
     result.submittedMobile = true;
-    request.requester.submittedMobile = true;
+    // request.requester.submittedMobile = true;
     await axios
-      .put(`http://localhost:4000/users/${requester_id}`, result)
+      .put(`http://localhost:8081/person/updateRequest/${requester_id}`, result)
       .then((res) => {
         // localStorage.setItem("user", res.data)
         console.log(res.data);
         localStorage.setItem("user", JSON.stringify(res.data));
         navigate("/dashboard");
-      });
-    await axios.put(
-      `http://localhost:4000/resignation_requests/${request_id}`,
-      request
-    );
+      }).catch((err) => console.log(err))
+    // await axios.put(
+    //   `http://localhost:8081/person/updateRequest/${request_id}`,
+    //   request
+    // );
     window.location.reload()
   };
 
   const handleAC = async (requester_id, request_id) => {
     let result = {};
-    let request = {};
+    // let request = {};
     await axios
-      .get(`http://localhost:4000/users/${cuur_user.id}`)
+      .get(`http://localhost:8081/person/${requester_id}`)
       .then((res) => (result = res.data));
-    await axios
-      .get(`http://localhost:4000/resignation_requests/${request_id}`)
-      .then((res) => {
-        request = res.data;
-        console.log(res.data);
-      });
+    // await axios
+    //   .get(`http://localhost:4000/resignation_requests/${request_id}`)
+    //   .then((res) => {
+    //     request = res.data;
+    //     console.log(res.data);
+    //   });
     result.submittedAccess = true;
-    request.requester.submittedAccess = true;
+    // request.requester.submittedAccess = true;
     await axios
-      .put(`http://localhost:4000/users/${requester_id}`, result)
+      .put(`http://localhost:8081/person/updateRequest/${requester_id}`, result)
       .then((res) => {
         // localStorage.setItem("user", res.data)
         console.log(res.data);
         localStorage.setItem("user", JSON.stringify(res.data));
         navigate("/dashboard");
-      });
-    await axios.put(
-      `http://localhost:4000/resignation_requests/${request_id}`,
-      request
-    );
+      }).catch((err) => console.log(err));
+    // await axios.put(
+    //   `http://localhost:4000/resignation_requests/${request_id}`,
+    //   request
+    // );
     window.location.reload()
   };
 
   const handleMoney = async (requester_id, request_id) => {
     let result = {};
-    let request = {};
+    // let request = {};
     await axios
-      .get(`http://localhost:4000/users/${cuur_user.id}`)
+      .get(`http://localhost:8081/person/${requester_id}`)
       .then((res) => (result = res.data));
+    // await axios
+    //   .get(`http://localhost:4000/resignation_requests/${request_id}`)
+    //   .then((res) => {
+    //     request = res.data;
+    //     console.log(res.data);
+    //   });
+    result.balanceMoney = 0.0;
+    // request.requester.balanceMoney = 0;
     await axios
-      .get(`http://localhost:4000/resignation_requests/${request_id}`)
-      .then((res) => {
-        request = res.data;
-        console.log(res.data);
-      });
-    result.balanceMoney = 0;
-    request.requester.balanceMoney = 0;
-    await axios
-      .put(`http://localhost:4000/users/${requester_id}`, result)
+      .put(`http://localhost:8081/person/updateRequest/${requester_id}`, result)
       .then((res) => {
         // localStorage.setItem("user", res.data)
         console.log(res.data);
         localStorage.setItem("user", JSON.stringify(res.data));
         navigate("/dashboard");
-      });
-    await axios.put(
-      `http://localhost:4000/resignation_requests/${request_id}`,
-      request
-    );
+      }).catch((err) => console.log(err))
+    // await axios.put(
+    //   `http://localhost:4000/resignation_requests/${request_id}`,
+    //   request
+    // );
     window.location.reload()
   };
 
   const handleBenifits = async (requester_id, request_id) => {
     let result = {};
-    let request = {};
+    // let request = {};
     await axios
-      .get(`http://localhost:4000/users/${cuur_user.id}`)
+      .get(`http://localhost:8081/person/${requester_id}`)
       .then((res) => (result = res.data));
+    // await axios
+    //   .get(`http://localhost:4000/resignation_requests/${request_id}`)
+    //   .then((res) => {
+    //     request = res.data;
+    //     console.log(res.data);
+    //   });
+    result.balanceBenifits = 0.0;
+    // request.requester.balanceBenifits = 0;
     await axios
-      .get(`http://localhost:4000/resignation_requests/${request_id}`)
-      .then((res) => {
-        request = res.data;
-        console.log(res.data);
-      });
-    result.balanceBenifits = 0;
-    request.requester.balanceBenifits = 0;
-    await axios
-      .put(`http://localhost:4000/users/${requester_id}`, result)
+      .put(`http://localhost:8081/person/updateRequest/${requester_id}`, result)
       .then((res) => {
         // localStorage.setItem("user", res.data)
         console.log(res.data);
         localStorage.setItem("user", JSON.stringify(res.data));
         navigate("/dashboard");
-      });
-    await axios.put(
-      `http://localhost:4000/resignation_requests/${request_id}`,
-      request
-    );
+      }).catch((err) => console.log(err));
+    // await axios.put(
+    //   `http://localhost:4000/resignation_requests/${request_id}`,
+    //   request
+    // );
     window.location.reload()
   };
 
@@ -168,36 +168,36 @@ export default function FunctionTray({ requestsArr }) {
   return (
     <div className="mt-5">
       {requestsArr.map((each) => {
-        if (each?.employeeID === cuur_user.employeeID) {
+        if (each?.person?.employeeID === cuur_user.employeeID) {
           return (
             <div key={each?.id} className="container">
               <div className="d-flex flex-wrap gap-2">
                 <button
-                  onClick={() => handleSL(each?.requester?.id, each?.id)}
+                  onClick={() => handleSL(each?.person?.employeeID, each?.resignation?.employeeID)}
                   className="btn btn-primary ternary-bg"
                 >
                   Surrender Laptop
                 </button>
                 <button
-                  onClick={() => handleSM(each?.requester?.id, each?.id)}
+                  onClick={() => handleSM(each?.person?.employeeID, each?.resignation?.employeeID)}
                   className="btn btn-primary ternary-bg"
                 >
                   Surrender Mobile
                 </button>
                 <button
-                  onClick={() => handleAC(each?.requester?.id, each?.id)}
+                  onClick={() => handleAC(each?.person?.employeeID, each?.resignation?.employeeID)}
                   className="btn btn-primary ternary-bg"
                 >
                   Surrender Access Card
                 </button>
                 <button
-                  onClick={() => handleMoney(each?.requester?.id, each?.id)}
+                  onClick={() => handleMoney(each?.person?.employeeID, each?.resignation?.employeeID)}
                   className="btn btn-primary ternary-bg"
                 >
                   Pay the owed money (balance pay)
                 </button>
                 <button
-                  onClick={() => handleBenifits(each?.requester?.id, each?.id)}
+                  onClick={() => handleBenifits(each?.person?.employeeID, each?.resignation?.employeeID)}
                   className="btn btn-primary ternary-bg"
                 >
                   Pay the owed money (balance benefits)
