f5b8388d23164578b6ff8a2bb34107cc0597be7e
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2023-10-08 14:41:53 +0530

    feat: add tests for cloud config server
    - checks whether the cloud-config-server runs successfully

--- backend/cloud-config-server/cloud-config-server/src/test/java/com/Natwest/cloudconfigserver/CloudConfigServerApplicationTests.java
+++ backend/cloud-config-server/cloud-config-server/src/test/java/com/Natwest/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -2,12 +2,17 @@ package com.Natwest.cloudconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootTest
+@EnableConfigServer
+@EnableEurekaClient
 class CloudConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		CloudConfigServerApplication.main(new String[] {});
 	}
 
 }
