fd022adc711a6e3cee74f90e6cba9c40ebe7af56
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2023-10-08 14:48:39 +0530

    feat: add tests for cloud API gateway
    - checks whether gateway starts without any failures

--- backend/cloud-gateway/cloud-gateway/src/test/java/com/Natwest/cloudgateway/CloudGatewayApplicationTests.java
+++ backend/cloud-gateway/cloud-gateway/src/test/java/com/Natwest/cloudgateway/CloudGatewayApplicationTests.java
@@ -2,12 +2,17 @@ package com.Natwest.cloudgateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootTest
+@EnableEurekaClient
+@CrossOrigin(origins = "*")
 class CloudGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
+		CloudGatewayApplication.main(new String[] {});
 	}
 
 }
